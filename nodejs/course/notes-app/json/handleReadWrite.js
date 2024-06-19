b6e837af5350cc5a21cad0550c4bf93fd16005a2
kevit-shrut-sureja <shrut.sureja@kevit.io>
2024-06-19 12:18:20 +0530

    modified the notes.json formate

--- nodejs/course/notes-app/json/handleReadWrite.js
+++ nodejs/course/notes-app/json/handleReadWrite.js
@@ -12,7 +12,7 @@ function readJSON(){
 
 function writeJSON(data){
     try {
-        const dataJSON = JSON.stringify(data);
+        const dataJSON = JSON.stringify(data, null, 2); // so that we can see the data in a more good way
         fs.writeFileSync(path, dataJSON)
         return true;
     } catch (error) {
