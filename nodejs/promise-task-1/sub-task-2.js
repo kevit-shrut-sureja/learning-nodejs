0625d86db06b20facba4f9ef905d1dc071433618
kevit-shrut-sureja <shrut.sureja@kevit.io>
2024-06-20 10:53:09 +0530

    promise task 2 incomplete

--- nodejs/promise-task-1/sub-task-2.js
+++ nodejs/promise-task-1/sub-task-2.js
@@ -12,50 +12,39 @@ function getTime(){
 // }
 
 // Set - 2
-// const time = {
-//     A : 2000,
-//     B : 3000,
-//     C : 1000,
-//     D : 1000,
-// }
-
-// Set - 3
 const time = {
-    A : 1000,
-    B : 1000,
+    A : 2000,
+    B : 3000,
     C : 1000,
     D : 1000,
 }
+
+// Set - 3
+// const time = {
+//     A : 1000,
+//     B : 1000,
+//     C : 1000,
+//     D : 1000,
+// }
 
 function executeWithTimeout(letter, time){
     return new Promise((resolve, reject) => {
         console.log(`Executed at: ${getTime()}\t with the letter: ${letter} and time: ${time}`);
         setTimeout(() => {
             console.log(`Executed at: ${getTime()}\t with the letter: ${letter} and time: ${time}\t --executed`);
-            // console.log(`SetTimeout executed at: ${getTime()} with the letter: ${letter}`)
             resolve()
         }, time);
     })
 }
 
-const execution = async () => {
-
-    const a = executeWithTimeout('A',time.A);
-    const d = executeWithTimeout('D',time.D);
-    Promise.all([a,d]).then(() =>  executeWithTimeout('C', time.C));
-    a.then(() =>  executeWithTimeout('B', time.B));
-
-}
-
 const main = async () => {
     console.log('Starting at:', new Date().toTimeString());
-    // await execution();
 
     const promiseA = executeWithTimeout('A',time.A);
     const promiseD = executeWithTimeout('D',time.D);
-
+    // after a ends starts
     const promiseB = promiseA.then(() =>  executeWithTimeout('B', time.B));
-
+    // after a and d ends
     const promiseC = Promise.all([promiseA,promiseD]).then(() =>  executeWithTimeout('C', time.C));
     
     Promise.all([promiseC, promiseB]).then(() => console.log('Completed at:', new Date().toTimeString()));
