7fbd1701062d3c8d74c334729aac0227fe7f5bc3
kevit-shrut-sureja <shrut.sureja@kevit.io>
2024-07-10 16:48:23 +0530

    More on the mongoDB ObjectIDs

--- nodejs/course/task-manager/mongodb.js
+++ nodejs/course/task-manager/mongodb.js
@@ -1,8 +1,15 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 
 const uri = "mongodb://127.0.0.1:27017/";
 
 const client = new MongoClient(uri);
+const id = new ObjectId()
+console.log(id);
+console.log(id.toHexString());
+console.log(id.toHexString().length);
+console.log(id.id);
+console.log(id.id.length);
+console.log(id.getTimestamp());
 
 const databaseName = "task-manager"
 
@@ -11,23 +18,26 @@ async function run() {
     // Establish and verify connection to the database
     const db = client.db(databaseName);
     const usersCollection = db.collection("tasks");
-    const res = await usersCollection.insertMany([
-      {
-        description : "task 1",
-        completed : true
-      },
-      {
-        description : "task 3",
-        completed : true
-      },
-      {
-        description : "task 2",
-        completed : false
-      },
-    ]);
-    if(!res.acknowledged){
-      console.log("was not able to push the tasks");
-    }
+    // const res1 = await usersCollection.insertMany([
+    //   {
+    //     description : "task 1",
+    //     completed : true
+    //   },
+    //   {
+    //     description : "task 3",
+    //     completed : true
+    //   },
+    //   {
+    //     description : "task 2",
+    //     completed : false
+    //   },
+    // ]);
+    // if(!res1.acknowledged){
+    //   console.log("was not able to push the tasks");
+    // }
+
+    const res = await usersCollection.findOne({})
+    console.log(res._id.getTimestamp().toLocaleString());
     console.log(res);
   } finally {
     // Close the database connection on completion or error
