de4b415a59ab930f82fc6c16bcdb1d484b7bb191
kevit-shrut-sureja <shrut.sureja@kevit.io>
2024-07-16 12:28:52 +0530

    Completed section 12

--- nodejs/course/task-manager/src/index.js
+++ nodejs/course/task-manager/src/index.js
@@ -20,3 +20,18 @@ app.use(taskRoutes)
 app.listen(port, () => {
     console.log(`SERVER : Live on the http://localhost:${port}`);
 })
+
+// const Task = require('./models/tasks.js')
+// const User = require('./models/user.js')
+
+// const main = async () => {
+    // const task = await Task.findById('6696078f9bd2a46e67f90331');
+    // await task.populate('author')
+    // console.log(task);
+
+    // const user = await User.findById('6696077c9bd2a46e67f90320');
+    // await user.populate('tasks')
+    // console.log(user.tasks);
+// }
+
+// main()
